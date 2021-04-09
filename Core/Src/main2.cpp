492c758ba39c4d5057e14d92798ba5a4667a9355
VersBinarii <versbinarii@gmail.com>
2021-04-09 23:24:27 +0200

    Incorporate all API changes

--- Core/Src/main2.cpp
+++ Core/Src/main2.cpp
@@ -7,93 +7,72 @@
 #include "tim.h"
 #include "gpio.h"
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
 #include <bme_280.h>
+#include <fonts/arialBlack_20ptFontInfo.h>
+#include <fonts.h>
 #include <touch.h>
 #include "GFX_color.h"
-#include "fonts/fonts.h"
 #include "lcd.h"
-#include "logo.h"
-#include "out.h"
 
 #define BME280_ADDRESS 0x76
 
-BME::BME280 bme280;
-ILI9341::LCD lcd;
-GFX_Color::GFX gfx;
-XPT2046::TouchPanel tp;
+ BME::BME280 bme280;
+ XPT2046::TouchPanel tp;
 
+ void main2 (void) {
+     tp.init (hspi1, EXTI2_IRQn);
+     ILI9341::LCD lcd{ &hspi5 };
+     Fonts::Font font{ arialBlack_20ptFontInfo };
+     GFX_Color::GFX gfx{ lcd, font };
 
-void main2(void){
-  /* USER CODE BEGIN 1 */
-  float temperature;
-  float pressure;
-  float humidity;
+     float temperature;
+     float pressure;
+     float humidity;
 
-  char msg[32];
-  /* USER CODE END 1 */
+     char msg[32];
 
-  /* MCU Configuration--------------------------------------------------------*/
+     HAL_TIM_PWM_Start (&htim9, TIM_CHANNEL_2);
+     __HAL_TIM_SET_COMPARE (&htim9, TIM_CHANNEL_2, 700);
 
-  HAL_TIM_PWM_Start(&htim9, TIM_CHANNEL_2);
+     // BME::BME280_init (bme280, &hi2c1, BME280_ADDRESS);
 
-  ILI9341::init (lcd, &hspi5);
+     // XPT2046::init (tp, hspi1, EXTI2_IRQn);
 
-  GFX_Color::init (gfx, lcd);
-  ILI9341::draw_image (lcd, 0, 5, logo1, 240, 72);
-  GFX_Color::set_font (gfx, font_8x5);
-  // BME::BME280_init (bme280, &hi2c1, BME280_ADDRESS);
+     // XPT2046::calibrate (tp, gfx);
 
-  //XPT2046::init (tp, hspi1, EXTI2_IRQn);
+     lcd.clear_display (ILI9341::Color::GREEN);
 
-  //XPT2046::calibrate (tp, gfx);
+     XPT2046::Point p;
 
-  //ILI9341::clear_display (lcd, ILI9341::Color::PURPLE);
+     gfx.draw_string ("Dupa sraka", 20, 50, ILI9341::Color::PURPLE,
+                      ILI9341::Color::WHITE, Fonts::FontBackground::COLOR);
+     gfx.draw_string ("Ale jaja japierpapier", 20, 100, ILI9341::Color::PURPLE,
+                      ILI9341::Color::WHITE,
+                      Fonts::FontBackground::TRANSPARENT);
 
-  //XPT2046::Point p;
+     /* Infinite loop */
+     while (1) {
+         //      BME::BME280_read_temp (bme280, temperature);
+         //    BME::BME280_read_pressure (bme280, pressure);
+         //   BME::BME280_read_humidity (bme280, humidity);
+         //   sprintf (msg, "Temp: %.2f C", temperature);
+         //   GFX_DrawString (0, 130, msg, ILI9341_RED);
+         //  sprintf (msg, "Pressure: %.2f Pa", pressure);
+         //   GFX_DrawString (0, 140, msg, ILI9341_RED);
+         //  sprintf (msg, "Hum: %.2f%%", humidity);
+         //    GFX_DrawString (0, 150, msg, ILI9341_RED);
 
-
-
-  /* Infinite loop */
-  while (1) {
-      //    BME::BME280_read_temp (bme280, temperature);
-      //   BME::BME280_read_pressure (bme280, pressure);
-      //  BME::BME280_read_humidity (bme280, humidity);
-      // sprintf (msg, "Temp: %.2f C", temperature);
-      // GFX_DrawString (0, 130, msg, ILI9341_RED);
-      // sprintf (msg, "Pressure: %.2f Pa", pressure);
-      //  GFX_DrawString (0, 140, msg, ILI9341_RED);
-      // sprintf (msg, "Hum: %.2f%%", humidity);
-      //  GFX_DrawString (0, 150, msg, ILI9341_RED);
-      //  ILI9341_ClearDisplay (ILI9341_PURPLE);
-
-	  uint16_t i;
-
-	  for(i = 0; i < 1000; i+=25)
-	  {
-		  __HAL_TIM_SET_COMPARE(&htim9, TIM_CHANNEL_2, i);
-		  HAL_Delay(300);
-	  }
-
-	  for(i = 1000; i > 0; i-=25)
-	  {
-		  __HAL_TIM_SET_COMPARE(&htim9, TIM_CHANNEL_2, i);
-		  HAL_Delay(300);
-	  }
-
-
-     // XPT2046::run (tp);
-     // if (XPT2046::is_touched (tp)) {
-     //     p = XPT2046::get_touch_point (tp);
-      //    ILI9341::write_pixel (lcd, p.x, p.y, ILI9341::Color::WHITE);
-      //}
-  }
+          tp.run ();
+          if (tp.is_touched ()) {
+              p = tp.get_touch_point ();
+              lcd.write_pixel (p.x, p.y, ILI9341::Color::WHITE);
+          }
+     }
 }
 
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){
 	if(GPIO_Pin == TOUCH_IRQ_Pin){
-		XPT2046::IRQ_handler(tp);
-	}
+            tp.IRQ_handler ();
+        }
 }
 
